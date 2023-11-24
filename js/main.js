ceb56607abd4b24a5c47efca96f75f3daa2c04f5
Moaz Elgandy <moazhazem2005@icloud.com>
2023-11-24 21:09:23 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -110,3 +110,18 @@ window.onscroll = function () {
     started = true;
   }
 };
+// Type effect
+document.addEventListener("DOMContentLoaded", function () {
+  const typed = document.querySelector(".typed");
+  if (typed) {
+    let typed_strings = typed.getAttribute("data-typed-items");
+    typed_strings = typed_strings.split(",");
+    new Typed(".typed", {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000,
+    });
+  }
+});
